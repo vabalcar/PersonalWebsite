22b2ce17135a1c7efc40f10ad2ed1882bafc17d9
vabalcar <vabalcar@gmail.com>
2018-12-23 18:37:21 +0100

    Python hello world version

---
+++ cgi-bin/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import cgi
+
+print ("Content-Type: text/html")
+print ("")
+print ("<meta charset=\"UTF-8\">")
+print ("<p>Příliš žluťoučký kůň úpěl ďábelské ódy.</p>")
